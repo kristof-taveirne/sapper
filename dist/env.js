35555d4cb8f7b3b75841e253b5ae0f80218c7a3a
kristof <kristof.taveirne@gmail.com>
2021-07-23 08:50:45 +0200

    including the dist folder. I know I shouldn't but rush is not picking up the prepare script.

---
+++ dist/env.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.dev = void 0;
+exports.src = void 0;
+exports.dest = void 0;
+const set_dev = (_) => exports.dev = _;
+const set_src = (_) => exports.src = _;
+const set_dest = (_) => exports.dest = _;
+
+exports.set_dest = set_dest;
+exports.set_dev = set_dev;
+exports.set_src = set_src;
+//# sourceMappingURL=env.js.map
